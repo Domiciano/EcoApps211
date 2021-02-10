8254129e050488b4566f2737298a19fd2e43e903
Domiciano <domiciano.rincon@correounivalle.edu.co>
2021-02-10 18:14:25 -0500

    Semana 2

--- Semana 1/Código/EAS1ACode-master/app/src/main/java/edu/co/icesi/semana1ecoa/MainActivity.java
+++ Semana 1/Código/EAS1ACode-master/app/src/main/java/edu/co/icesi/semana1ecoa/MainActivity.java
@@ -2,13 +2,23 @@ package edu.co.icesi.semana1ecoa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    String alfa = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        miFuncion(this);
     }
+
+    public void miFuncion( AppCompatActivity param1 ){
+
+    }
+
 }
